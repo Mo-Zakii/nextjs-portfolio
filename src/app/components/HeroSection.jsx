d7adb7a0b8ffbaeb95af7663d7516a3b4b9950f5
Mohamed Zaki <Mohamed.MEM@eco.suezuni.edu.eg>
2024-08-21 00:48:04 +0300

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,13 +22,13 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Mohamed Zaki",
                 1000,
                 "Web Developer",
                 1000,
-                "Mobile Developer",
+                "Economist",
                 1000,
-                "UI/UX Designer",
+                "data analyst",
                 1000,
               ]}
               wrapper="span"
